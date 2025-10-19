30f388e1e59169a275c9a59010bb2aa0f9d4ec8a
hugeorange <18440746+hugeorange@users.noreply.github.com>
2025-10-19 17:46:09 +0800

    update

--- demo.js
+++ demo.js
@@ -3,6 +3,6 @@ const { deploy } = require("./deploy.cjs")
 deploy("demo", {
 	KEY_PATH: "deploy/demo-google-cloud.pem", // SSH 私钥，production需要将其 ignore,
 	REMOTE_USER: "lei",
-	REMOTE_HOST: "35.238.37.208",
+	REMOTE_HOST: "35.238.xx",
 	REMOTE_BASE: "/var/www/hello",
 })
